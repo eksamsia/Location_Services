1b8249a4cc22accfc66c93d9fa68d8251f9a26f7
eksamsia <ekasamsiati03@mail.ugm.ac.id>
2022-04-18 11:24:42 +0700

    task 2 :show reall address

--- app/src/main/java/com/example/walkmyandroid1/MainActivity.java
+++ app/src/main/java/com/example/walkmyandroid1/MainActivity.java
@@ -17,7 +17,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted{
 
     private TextView mLocationTextView;
     private Button mLocationButton;
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         mLocationButton = findViewById(R.id.button_location);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
                 getLocation();
             }
@@ -44,33 +45,48 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case REQUEST_LOCATION_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getLocation();
-                } else {
+                }
+                else {
                     Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
                 }
         }
     }
 
     private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_PERMISSION);
-        } else {
-             mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                 @Override
-                 public void onSuccess(Location location) {
-                     if (location != null) {
-                         String result = "Latitude " + String.valueOf(location.getLatitude()) +
-                                 "\n Longitude " + String.valueOf(location.getLongitude());
-
-                         mLocationTextView.setText(result);
-                     } else {
-                         mLocationTextView.setText("Location not found");
-                     }
-                 }
-             });
         }
+        else {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+
+                    if (location != null) {
+
+//                        String result = "Latitude " + String.valueOf(location.getLatitude())
+//                                + "\n Longitude " + String.valueOf(location.getLongitude());
+//
+//                        mLocationTextView.setText(result);
+
+
+                        new FetchAddressTask(MainActivity.this, MainActivity.this).execute(location);
+                    }
+                    else {
+                        mLocationTextView.setText("Location not found");
+                    }
+                }
+            });
+        }
+    }
+
+
+    @Override
+    public void OnTaskCompleted(String result) {
+        mLocationTextView.setText(result);
     }
 
 }
